780f5ac58335cbe7ffcef3f700bf9f79c1525f6d
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-09-09 00:33:12 -0400

    quickly layout project direction.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    network request, afterwich local storage; requests will occur below:
+    <br/><br/>https://api.github.com/users/michaeldimmitt/repos
   </div>
 </template>
 
